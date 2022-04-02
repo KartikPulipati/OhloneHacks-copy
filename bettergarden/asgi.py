892a43630fe03bea9d5a5dc48bcbb5c5a27cdc6a
Arhan Chaudhary <arhan.ch@gmail.com>
2022-04-02 13:02:20 -0700

    Change django to 4.0

--- bettergarden/asgi.py
+++ bettergarden/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for bettergarden project.
+ASGI config for bettergarden2 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bettergarden.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bettergarden2.settings')
 
 application = get_asgi_application()
