43aa572d05b98973a4189a160aad03b8462be43e
Arhan Chaudhary <arhan.ch@gmail.com>
2022-04-02 13:36:10 -0700

    Directory typo

--- bettergarden/urls.py
+++ bettergarden/urls.py
@@ -1,4 +1,4 @@
-"""bettergarden2 URL Configuration
+"""bettergarden URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
