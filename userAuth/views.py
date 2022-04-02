808551f87ef03505ecd01c2972de110184e773bd
Arhan Chaudhary <arhan.ch@gmail.com>
2022-04-02 14:46:06 -0700

    Render userAuth form

--- userAuth/views.py
+++ userAuth/views.py
@@ -1,10 +1,20 @@
+from glob import escape
 from django.shortcuts import render
 from django.views import View
-
+from userAuth.forms import GardenForm
 class LoginView(View):
-    
+
     def get(self, request):
-        return render(request, 'login.html')
+        self.garden_form = GardenForm()
+        return render(request, 'login.html', {'garden_form': self.garden_form})
 
     def post(self, request):
-        pass
+        self.garden_form = GardenForm(request.POST)
+        if self.garden_form.is_valid():
+            return self.valid_form(request)
+        else:
+            return self.invalid_form(request)
+
+    def valid_form(self, request):
+
+        return render(request, 'login.html', {'garden_form': self.garden_form})
