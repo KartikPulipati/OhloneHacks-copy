91d11250c9bbe43b14cca9777b76958782ba7745
Arhan Chaudhary <arhan.ch@gmail.com>
2022-04-03 08:20:38 -0700

    move css to base

--- display/views.py
+++ display/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
-def home(request):
-    return render(request, 'display/home.html')
+@login_required
+def home(requests):
+    if requests.method == "GET":
+        return render(requests, 'display/home.html')
+    else:
+        logout(requests)
+        return redirect(reverse("login"))
